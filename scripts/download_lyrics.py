6be97f6f5d4443271edb90c74e49a026412312d0
Max Mathless <iammax64@gmail.com>
2019-02-26 14:42:41 -0500

    Update download_lyrics.py
    Download_lyrics erroneously tried to iterate over an artist object when in fact it automatically does that when using save_lyrics, causing an error. Also moves lyrics text files to their own directory

--- scripts/download_lyrics.py
+++ scripts/download_lyrics.py
@@ -5,12 +5,13 @@ the songs of a specified artist(s) to a specified directory in a
 future PR.
 """
 import sys
-
+import shutil
+import glob
 import lyricsgenius
 
 
 def download_lyrics(api_key):
-    bands = [
+	bands = [
         "Iron Maiden",
         "Black Sabbath",
         "Blind Guardian",
@@ -26,24 +27,30 @@ def download_lyrics(api_key):
         "Dragonforce",
     ]
 
-    genius = lyricsgenius.Genius(api_key)
-
-    for band in bands:
+	genius = lyricsgenius.Genius(api_key)
+	
+	for band in bands:
         # We're using nested try/except blocks here for 2 reasons:
         # 1. the song downloading can fail from a connection error
         #    (could use a library like tenacity for retrying logic)
         # 2. song titles with forward slashes on them will not be saved
         #    (we can figure out how to handle this too)
-        try:
-            songs = genius.search_artist(band, sort="popularity", max_songs=50)
-            for song in songs:
-                try:
-                    song.save_lyrics(extension="txt")
-                except Exception:
-                    pass
-        except Exception:
-            pass
-
+		try:
+			songs = genius.search_artist(band, sort="popularity", max_songs=50)
+			try:
+				songs.save_lyrics(extension="txt")
+			except Exception:
+				print ("Couldn't save lyrics")    
+		except Exception:
+			print ("Couldn't find band")
+	#Move the lyrics file to lyrics folder
+	files = glob.glob('./lyrics_*')
+	try:
+		os.mkdir('lyrics')
+	except:
+		pass
+	for lyricsfile in files:
+		shutil.move('{0}'.format(lyricsfile), 'lyrics/{0}'.format(lyricsfile))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
