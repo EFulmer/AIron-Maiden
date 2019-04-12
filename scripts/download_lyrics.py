da07bddc8fd3a47c067459cc21032b3381bf0a75
iammax <iammax64@gmail.com>
2019-04-12 15:19:44 -0400

    BUG: Made download_lyrics.py properly save into text files

--- scripts/download_lyrics.py
+++ scripts/download_lyrics.py
@@ -9,22 +9,10 @@ import shutil
 import glob
 import lyricsgenius
 
-
 def download_lyrics(api_key):
     bands = [
         "Iron Maiden",
-        "Black Sabbath",
-        "Blind Guardian",
-        "Judas Priest",
-        "Rainbow",
-        "Metallica",
-        "Motorhead",
-        "Megadeth",
-        "Manilla Road",
-        "Thin Lizzy",
-        "Led Zeppelin",
-        "Manowar",
-        "Dragonforce",
+      
     ]
 
     genius = lyricsgenius.Genius(api_key)
@@ -36,7 +24,7 @@ def download_lyrics(api_key):
         # 2. song titles with forward slashes on them will not be saved
         #    (we can figure out how to handle this too)
 	    try:
-		    songs = genius.search_artist(band, sort="popularity", max_songs=50)
+		    songs = genius.search_artist(band, sort="popularity", max_songs=1)
 		    try:
 			    songs.save_lyrics(extension="txt")
 		    except Exception:
@@ -44,7 +32,7 @@ def download_lyrics(api_key):
 	    except Exception:
 		    print ("Couldn't find band")
     #Move the lyrics file to lyrics folder
-    files = glob.glob('./lyrics_*')
+    files = set(glob.glob("./lyrics_*")) - set(glob.glob("./*.py")) #Lyrics API saves files as lyrics_$songname.txt; make sure not to move lyrics_frequency.py by accident
     try:
         os.mkdir('lyrics')
     except:
