8e512e2f6c811c92a0a999d47fd5d485dad8a580
Max Mathless <iammax64@gmail.com>
2019-04-12 19:22:36 -0400

    BUG: Made download_lyrics.py properly save into text files
    FORMATTING
    
    FORMATTING: Improved readability of download_lyrics.py

--- scripts/download_lyrics.py
+++ scripts/download_lyrics.py
@@ -4,27 +4,15 @@ Should be abstracted out to something that can dedupe, clean, and save
 the songs of a specified artist(s) to a specified directory in a
 future PR.
 """
+import os
 import sys
-
+import shutil
+import glob
 import lyricsgenius
 
 
 def download_lyrics(api_key):
-    bands = [
-        "Iron Maiden",
-        "Black Sabbath",
-        "Blind Guardian",
-        "Judas Priest",
-        "Rainbow",
-        "Metallica",
-        "Motorhead",
-        "Megadeth",
-        "Manilla Road",
-        "Thin Lizzy",
-        "Led Zeppelin",
-        "Manowar",
-        "Dragonforce",
-    ]
+    bands = ["Iron Maiden"]
 
     genius = lyricsgenius.Genius(api_key)
 
@@ -35,14 +23,23 @@ def download_lyrics(api_key):
         # 2. song titles with forward slashes on them will not be saved
         #    (we can figure out how to handle this too)
         try:
-            songs = genius.search_artist(band, sort="popularity", max_songs=50)
-            for song in songs:
-                try:
-                    song.save_lyrics(extension="txt")
-                except Exception:
-                    pass
+            songs = genius.search_artist(band, sort="popularity", max_songs=1)
+            try:
+                songs.save_lyrics(extension="txt")
+            except Exception:
+                print("Couldn't save lyrics")
         except Exception:
-            pass
+            print("Couldn't find band")
+    # Move the lyrics file to lyrics folder
+    files = set(glob.glob("./lyrics_*")) - set(
+        glob.glob("./*.py")
+    )  # Lyrics API saves files as lyrics_$songname.txt; make sure not to move lyrics_frequency.py by accident
+    try:
+        os.mkdir("lyrics")
+    except:
+        pass
+    for lyricsfile in files:
+        shutil.move("{0}".format(lyricsfile), "lyrics/{0}".format(lyricsfile))
 
 
 if __name__ == "__main__":
