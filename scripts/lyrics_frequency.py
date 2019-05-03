f0ed57e4e28d5d10e4170f4e8a7486adc6351bf4
Max Mathless <iammax64@gmail.com>
2019-05-03 15:05:32 -0400

    Fix: Updated lyrics_frequency.py to look in the correct place for lyrics data. Additionally it now outputs each filename it looks at, for easier output reading

--- scripts/lyrics_frequency.py
+++ scripts/lyrics_frequency.py
@@ -1,13 +1,11 @@
 from collections import Counter
-from pathlib import Path
-
-data_dir = Path("./data/")
+import glob
 
 all_words = []
 
-for song_file in data_dir.glob("lyrics_manowar_*.txt"):
+for song_file in glob.glob("./lyrics/*"):
     with open(song_file, "r") as f:
         all_words.extend(f.read().split())
-
         hist = Counter(all_words)
+        print(f"Name of file: {song_file}")
         print(hist.most_common(50))
